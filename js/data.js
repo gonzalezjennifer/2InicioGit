4de0d5c122ce6bb635ab7a73de96132db69742be
Jennifer <gnzlezjennifer@gmail.com>
2023-02-02 11:39:22 -0600

    Se agregaron archivos JS

---
+++ js/data.js
@@ -0,0 +1,32 @@
+const songs = [
+    {
+        name: 'Song 1',
+        path: '../music/Song 1.mp3',
+        artist: 'no sabo',
+        cover: '../images/cover 1.png'
+    },
+    {
+        name: 'Song 2',
+        path: '../music/Song 2.mp3',
+        artist: 'no sabo',
+        cover: '../images/cover 2.png'
+    },
+    {
+        name: 'Song 3',
+        path: '../music/Song 3.mp3',
+        artist: 'no sabo',
+        cover: '../images/cover 3.png'
+    },
+    {
+        name: 'Song 4',
+        path: '../music/Song 4.mp3',
+        artist: 'no sabo',
+        cover: '../images/cover 4.png'
+    },
+    {
+        name: 'Song 5',
+        path: '../music/Song 5.mp3',
+        artist: 'no sabo',
+        cover: '../images/cover 5.png'
+    }
+]
